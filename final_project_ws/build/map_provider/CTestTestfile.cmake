3c924f897f23079ec8720433d48a25a3143ebf8b
Aleksei Korotchenko <77253650+aleksei089@users.noreply.github.com>
2021-01-17 15:47:59 -0800

    Delete final_project_ws directory

--- final_project_ws/build/map_provider/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alex/final_project_ws/src/map_provider
-# Build directory: /home/alex/final_project_ws/build/map_provider
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
